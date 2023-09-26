2eb6a4922f73cbbbc68391c1b44a404e3e327ae4
kizami <c0b2204952@edu.teu.ac.jp>
2023-09-26 15:31:23 +0900

    練習2

---
+++ pygame_image.py
@@ -0,0 +1,28 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    img3 = pg.image.load("ex01/fig/3.png")
+    img3 = pg.transform.flip(img3,True,False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(img3 ,[0, 0])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
