25eeb8743ce441b44cb14bf34df9cbb169c7be07
kizami <c0b2204952@edu.teu.ac.jp>
2023-09-26 16:54:56 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_2 = pg.transform.flip(bg_img,True,False)
     img3 = pg.image.load("ex01/fig/3.png")
     img3 = pg.transform.flip(img3,True,False)
     img3_1 = pg.transform.rotozoom(img3, 3, 1.0)
@@ -18,13 +19,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bg_img_2, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         screen.blit(img3s[tmr%6] ,[300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(50)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
