e3975b51a867c6625fe91f16f617710f587b7448
kizami <c0b2204952@edu.teu.ac.jp>
2023-09-26 16:02:50 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -16,11 +16,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%1600
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
         screen.blit(img3s[tmr%2] ,[300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
